9501050f70676b6ebf9edecfc8d2b31e1bcf1686
bhavaygoel <goelbhavay@gmail.com>
2023-08-06 23:15:09 +0530

    Completed the CSS

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -7,10 +7,10 @@ function App() {
     <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand">
-
+          
         </div>
         <div className="current-operand">
-
+          
         </div>
       </div>
       <button className="span-two">AC</button>
